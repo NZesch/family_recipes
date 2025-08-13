3b1d351f0c3cf63c30f9a06ab4bc5c3260728a17
NZesch <nzesch@gmail.com>
2025-08-12 20:40:54 -0400

    feat: add GET and POST /instructions/:recipe_id

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+// RECIPE REQUESTS
 app.get('/recipes', async (req, res) => {
   try {
     const result = await client.query('SELECT * from recipes;');
@@ -39,14 +40,11 @@ app.get('/recipes', async (req, res) => {
 app.post('/recipes', async (req, res) => {
   const { name, num_instructions } = req.body;
   recipe_id = uuidv4();
-  instruction_id = uuidv4();
   try {
     const result = await client.query(
-      'INSERT INTO recipes (recipe_id, instruction_id, name, num_instructions) VALUES ($1, $2, $3, $4) RETURNING *',
-      [recipe_id, instruction_id, name, num_instructions]
+      'INSERT INTO recipes (recipe_id, name, num_instructions) VALUES ($1, $2, $3) RETURNING *',
+      [recipe_id, name, num_instructions]
     );
-
-
 
     res.status(201).json(result.rows[0]);
   } catch (err) {
@@ -59,8 +57,35 @@ app.delete('/recipes', async (req, res) => {
     const result = await client.query(
       'DELETE FROM recipes'
     );
+
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message })
+  }
+})
+
+// INSTRUCTION REQUESTS
+
+app.get('/instructions/:recipe_id', async (req, res) => {
+  try {
+    const result = await client.query('SELECT * from instructions WHERE recipe_id = $1;', [req.params.recipe_id]);
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+})
+
+app.post('/instructions/:recipe_id', async (req, res) => {
+  const { details, instruction_number } = req.body;s
+  const recipe_id = req.params.recipe_id;
+  const instruction_id = uuidv4();
+  try {
+    const result = await client.query(
+      'INSERT INTO instructions (instruction_id, recipe_id, details, instruction_number) VALUES ($1, $2, $3, $4) RETURNING *',
+      [instruction_id, recipe_id, details, instruction_number]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 })
