dc147f8d9181b360fa17a0dccd5edf577a1a0b40
NZesch <nzesch@gmail.com>
2025-08-12 20:08:24 -0400

    feat: set up GET, POST, DELETE /recipes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,66 @@
-const { createServer } = require('node:http');
+const express = require('express');
+const { Client } = require('pg');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 
+const app = express();
+const port = 8800;
 const hostname = '127.0.0.1';
-const port = 5432;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'response/json');
-  res.end('Hello World');
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const client = new Client({
+  user: 'postgres',
+  password: 'nataliez5',
+  host:'127.0.0.1',
+  port: 5432,
+  database: 'postgres',
 });
 
-server.listen(port, hostname, () => {
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('DB connection error', err));
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+app.get('/recipes', async (req, res) => {
+  try {
+    const result = await client.query('SELECT * from recipes;');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+})
+
+app.post('/recipes', async (req, res) => {
+  const { name, num_instructions } = req.body;
+  recipe_id = uuidv4();
+  instruction_id = uuidv4();
+  try {
+    const result = await client.query(
+      'INSERT INTO recipes (recipe_id, instruction_id, name, num_instructions) VALUES ($1, $2, $3, $4) RETURNING *',
+      [recipe_id, instruction_id, name, num_instructions]
+    );
+
+
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+})
+
+app.delete('/recipes', async (req, res) => {
+  try {
+    const result = await client.query(
+      'DELETE FROM recipes'
+    );
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
